2cb6541bfd08c14fd75f2d4a62568958cf505638
aryan21x <aryanraval4014@gmail.com>
2024-05-13 02:44:18 -0600

    added a default navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import CustomNavbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar />
     </div>
   );
 }
