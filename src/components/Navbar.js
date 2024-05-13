65108aa6a5b1907872abd0e5cfbfed4815d8cdd8
aryan21x <aryanraval4014@gmail.com>
2024-05-13 04:32:59 -0600

    Minor change to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,29 @@
 
-import {Navbar,Nav,Container,NavDropdown} from 'react-bootstrap';
+import {Navbar,Nav,Container} from 'react-bootstrap';
 
 const CustomNavbar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Brand href="#home">
+            <img src='' alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggle-icon"></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
           </Nav>
+        <span className="navbar-text">
+            <div className="social-icon">
+                <a href="#"> <img src='' alt="1"></img> </a>
+                <a href="#"> <img src='' alt="2"></img> </a>
+                <a href="#"> <img src='' alt="3"></img> </a>
+            </div>
+        </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
