6d66456216c14d614b497fe6abaacaba5e9e43b6
aryan21x <aryanraval4014@gmail.com>
2024-05-14 12:16:23 -0600

    enhancing the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
 import {useState, useEffect} from "react";
 import {Navbar,Nav,Container} from 'react-bootstrap';
-import logo from "../assets/img/AnR.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import logo from "../assets/img/AnR2.svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const CustomNavbar = () => {
   const [activeLink, setActiveLink] = useState('home');
@@ -31,31 +30,39 @@ const CustomNavbar = () => {
   }
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>  {/* change to lg */}
-      <Container>
-        <Navbar.Brand href="#home" onClick={ () => onLogoClick()}>
-            <img src={logo} alt="logo" className="d-none d-lg-block"/>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-            <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('home')} >Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('skills')} >Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('projects')} >Projects</Nav.Link>
-          </Nav>
-        <span className="navbar-text">
-            <div className="social-icon">
-                <a href="#"> <img src={navIcon1} alt="1"></img> </a>
-                <a href="#"> <img src={navIcon2} alt="2"></img> </a>
-                <a href="#"> <img src={navIcon3} alt="3"></img> </a>
-            </div>
-            <button className="vvd" onClick={() => console.log('connect')}> <span>Let's Connect</span></button>
-        </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <head>
+        <title>Aryan Raval</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+          integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+      </head>
+      <Navbar expand="lg" className={scrolled ? "scrolled": ""}>  {/* change to lg */}
+        <Container>
+          <Navbar.Brand href="#home" onClick={ () => onLogoClick()}>
+              <img src={logo} alt="logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+              <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('home')} >Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('skills')} >Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('projects')} >Projects</Nav.Link>
+            </Nav>
+          <span className="navbar-text">
+              <div className="social-icon">
+                <a href="#"><FontAwesomeIcon icon={faGithub} className="colorChange"/></a>
+                <a href="#"><FontAwesomeIcon icon={faLinkedin} className="colorChange"/></a>
+                <a href="#"><FontAwesomeIcon icon={faInstagram} className="colorChange"/></a>
+              </div>
+              <button className="vvd" onClick={() => console.log('connect')}> <span>Let's Connect</span></button>
+          </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
