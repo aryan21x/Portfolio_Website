4609812119dbd38cb48612f800391bad1f6191c6
aryan21x <aryanraval4014@gmail.com>
2024-05-14 05:34:22 -0600

    fixed a small active bug that took forever to debug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from "react";
 import {Navbar,Nav,Container} from 'react-bootstrap';
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/AnR.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -22,24 +22,28 @@ const CustomNavbar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, [])
 
-  const onUpdateActivelink = (value) => {
+  const onUpdateActive = (value) => {
     setActiveLink(value);
+  }
+
+  const onLogoClick = () => {
+    setActiveLink('home');
   }
 
   return (
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>  {/* change to lg */}
       <Container>
-        <Navbar.Brand href="#home">
-            <img src={logo} alt="logo" />
+        <Navbar.Brand href="#home" onClick={ () => onLogoClick()}>
+            <img src={logo} alt="logo" className="d-none d-lg-block"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={ () => onUpdateActivelink('home')} >Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={ () => onUpdateActivelink('skills')} >Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={ () => onUpdateActivelink('projects')} >Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('home')} >Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('skills')} >Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'act navbar-link' : 'navbar-link'} onClick={ () => onUpdateActive('projects')} >Projects</Nav.Link>
           </Nav>
         <span className="navbar-text">
             <div className="social-icon">
