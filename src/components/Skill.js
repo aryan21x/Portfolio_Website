098fde4fdcd48d8e0234401a718ad2fba207ec54
aryan21x <aryanraval4014@gmail.com>
2024-05-16 00:33:47 -0600

    changed skills section

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,14 +1,17 @@
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import React from "../assets/img/React.png";
+import C from "../assets/img/C++.png";
+import Python from "../assets/img/python.png";
+import SQL from "../assets/img/SQL.png";
+import ASP from "../assets/img/c-sharp.png";
+import Web from "../assets/img/js.png";
+import Django from "../assets/img/Django.png";
 import colorSharp from "../assets/img/color-sharp.png"
 
 const Skills = () => {
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
           breakpoint: { max: 4000, min: 3000 },
           items: 5
         },
@@ -33,23 +36,35 @@ const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>I can do both frontend and backend website Development.<br></br> Here are the skills and languages I'm proficient in.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={Web} alt="Image" />
                                 <h5>Web Development</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <img src={React} alt="Image" />
+                                <h5>React.js</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <img src={Django} alt="Image" />
+                                <h5>Django</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={ASP} alt="Image" />
+                                <h5>ASP.net</h5>
+                            </div>
+                            <div className="item">
+                                <img src={C} alt="Image" />
+                                <h5>C++/C</h5>
+                            </div>
+                            <div className="item">
+                                <img src={Python} alt="Image" />
+                                <h5>Python</h5>
+                            </div>
+                            <div className="item recolor">
+                                <img src={SQL} alt="Image" />
+                                <h5>SQL</h5>
                             </div>
                         </Carousel>
                     </div>
